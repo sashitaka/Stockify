532831a40db3b9a11c7f06a97ca3828afacab039
cedma <cedma@CEDRICKPC>
2026-03-27 16:06:55 -0400

    création de MODELS et de User.cs, Transaction.cs, etc.

---
+++ code/Stockify/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Stockify.Models
+{
+    public class User : IdentityUser
+    {
+        public decimal Balance { get; set; } = 10000m;
+        public string Currency { get; set; } = "USD";
+        public string AccountStatus { get; set; } = "ACTIVE";
+    }
+}
