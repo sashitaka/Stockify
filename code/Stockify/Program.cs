2cd61ab7920e50af3164b52a6e820c215fa6ad07
cedma <cedma@CEDRICKPC>
2026-03-28 18:56:51 -0400

    modification de program.cs

--- code/Stockify/Program.cs
+++ code/Stockify/Program.cs
@@ -1,68 +1,27 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Stockify.Data;
-using Stockify.Models;
-using Stockify.Services;
+using Stockify.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// DB
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Identity
-builder.Services.AddDefaultIdentity<User>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-})
-.AddRoles<IdentityRole>()
-.AddEntityFrameworkStores<AppDbContext>();
-
-// Blazor
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-// HttpClient for Massive API
-builder.Services.AddHttpClient<StockService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["MassiveApi:BaseUrl"]!);
-    client.DefaultRequestHeaders.Add("x-api-key", builder.Configuration["MassiveApi:ApiKey"]);
-});
-
-// App services
-builder.Services.AddScoped<PortfolioService>();
-builder.Services.AddScoped<TransactionService>();
-builder.Services.AddScoped<WatchlistService>();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Seed roles
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    foreach (var role in new[] { "ADMIN", "CLIENT" })
-        if (!await roleManager.RoleExistsAsync(role))
-            await roleManager.CreateAsync(new IdentityRole(role));
-}
-
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
 
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
