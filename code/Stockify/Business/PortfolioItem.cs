ee9f9875babc161e4dda39072f467434577e4c74
AfterFaun2030 <familyhortas@hotmail.com>
2026-03-29 19:10:33 -0400

    page portfolio

--- code/Stockify/Business/PortfolioItem.cs
+++ code/Stockify/Business/PortfolioItem.cs
@@ -4,8 +4,10 @@
     {
         private float ownedValue;
         private float quantityOwned;
+        private decimal currentPrice;
 
         public float OwnedValue { get => ownedValue; set => ownedValue = value; }
         public float QuantityOwned { get => quantityOwned; set => quantityOwned = value; }
+        public decimal CurrentPrice { get => currentPrice; set => currentPrice = value; }
     }
 }
